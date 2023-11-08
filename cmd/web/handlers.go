9cd9d35e62645c79a5147549d8acc0cbac7cd480
kisi <kristijan4815@its.edu.rs>
2023-11-08 15:10:56 +0100

    update project structure and organization

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 )
@@ -34,15 +33,4 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
 	w.Header()["Date"] = nil
 	w.Write([]byte(`{"name" : "alex" }`))
-}
-
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
-
-	log.Print("Starting server on port: 4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
 }
