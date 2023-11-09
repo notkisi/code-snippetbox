b807ae6b330934fd4015a2ecb45de5da461df54b
notkisi <kristijan4815@its.edu.rs>
2023-11-09 17:25:57 +0100

    add config struct and restore css properties

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
+type config struct {
+	addr      string
+	staticDir string
+}
+
+var cfg config
+
 func main() {
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	flag.StringVar(&cfg.addr, "addr", ":4000", "Http network address")
+	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
+	flag.Parse()
 
+	fileServer := http.FileServer(http.Dir(cfg.staticDir))
 	mux := http.NewServeMux()
 
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
@@ -16,7 +27,7 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Print("Starting server on port: 4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("Starting server on port: %s\n", cfg.addr)
+	err := http.ListenAndServe(cfg.addr, mux)
 	log.Fatal(err)
 }
