eb920eb01aa202fe13a6007f3a6f0853119ba3f5
notkisi <kristijan4815@its.edu.rs>
2023-11-10 18:13:44 +0100

    add func for routes, add error logging helpers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -21,15 +21,13 @@ func (a *application) home(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		a.errorLog.Print(err.Error())
-		http.Error(w, "Internal Server ERROR", http.StatusInternalServerError)
+		a.serverError(w, err)
 		return
 	}
 
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		a.errorLog.Print(err.Error())
-		http.Error(w, "Internal Server ERROR", http.StatusInternalServerError)
+		a.serverError(w, err)
 		return
 	}
 
@@ -38,7 +36,7 @@ func (a *application) home(w http.ResponseWriter, r *http.Request) {
 func (a *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		a.notFound(w)
 		return
 	}
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
@@ -47,7 +45,7 @@ func (a *application) snippetView(w http.ResponseWriter, r *http.Request) {
 func (a *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		a.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
 
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,15 +12,15 @@ type config struct {
 	staticDir string
 }
 
-var cfg config
-
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	config   *config
 }
 
 func main() {
 
+	cfg := &config{}
 	flag.StringVar(&cfg.addr, "addr", ":4000", "Http network address")
 	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
 	flag.Parse()
@@ -28,21 +28,14 @@ func main() {
 	app := &application{
 		errorLog: log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
 		infoLog:  log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime),
+		config:   cfg,
 	}
-
-	fileServer := http.FileServer(http.Dir(cfg.staticDir))
-	mux := http.NewServeMux()
-
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
 	app.infoLog.Printf("Starting server on port: %s\n", cfg.addr)
 	srv := &http.Server{
 		Addr:     cfg.addr,
 		ErrorLog: app.errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 	err := srv.ListenAndServe()
 	app.errorLog.Fatal(err)
