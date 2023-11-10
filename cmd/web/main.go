e8c53d5d9b8b4bff97cead612987c1dfe9837eb5
notkisi <kristijan4815@its.edu.rs>
2023-11-10 17:36:24 +0100

    DI loggers into handlers

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -14,29 +14,36 @@ type config struct {
 
 var cfg config
 
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
 
 	flag.StringVar(&cfg.addr, "addr", ":4000", "Http network address")
 	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
 	flag.Parse()
 
-	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app := &application{
+		errorLog: log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
+		infoLog:  log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime),
+	}
 
 	fileServer := http.FileServer(http.Dir(cfg.staticDir))
 	mux := http.NewServeMux()
 
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	infoLog.Printf("Starting server on port: %s\n", cfg.addr)
+	app.infoLog.Printf("Starting server on port: %s\n", cfg.addr)
 	srv := &http.Server{
 		Addr:     cfg.addr,
-		ErrorLog: errorLog,
+		ErrorLog: app.errorLog,
 		Handler:  mux,
 	}
 	err := srv.ListenAndServe()
-	errorLog.Fatal(err)
+	app.errorLog.Fatal(err)
 }
